e18e682c3ecd0761b510467c433feac024a63b95
SlavaGrasiuk <slava_agent@mail.ru>
2017-09-12 17:11:10 +0300

    Created projects for VS2015 and QtCreator

---
+++ src/lab1.cpp
@@ -0,0 +1,9 @@
+#include "lab1.h"
+
+lab1::lab1(QWidget *parent): QMainWindow(parent) {
+	ui.setupUi(this);
+}
+
+lab1::~lab1() {
+
+}
---
+++ src/lab1.h
@@ -0,0 +1,18 @@
+#ifndef LAB1_H
+#define LAB1_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_lab1.h"
+
+class lab1 : public QMainWindow {
+	Q_OBJECT
+
+public:
+	lab1(QWidget *parent = 0);
+	~lab1();
+
+private:
+	Ui::lab1Class ui;
+};
+
+#endif // LAB1_H
